462061f9f776eae3a032232c3ef24ccb8b1d7fec
Marius Negoita <marius.negoita@mariusnegoita.org>
2022-11-22 20:26:18 -0500

    Removed an unused widget

--- base/widgets.py
+++ base/widgets.py
@@ -28,27 +28,6 @@ class RelatedFieldWidgetSingle(widgets.Select):
         return mark_safe(''.join(output))
       
 
-class RelatedFieldWidgetMultiple(widgets.SelectMultiple):
-
-    def __init__(self, related_model, related_url=None, *args, **kw):
-        super().__init__(*args, **kw)
-
-        if not related_url:
-            rel_to = related_model
-            info = (rel_to._meta.app_label, rel_to._meta.object_name.lower())
-            related_url = 'trips:%s_%s_add' % info
-
-        self.related_url = related_url
-
-    def render(self, name, value, *args, **kwargs):
-        self.related_url = reverse(self.related_url)
-        output = [super().render(name, value, *args, **kwargs)]
-        output.append('<a href="%s?_to_field=id&_popup=1" class="add-another" id="add_id_%s" onclick="return showAddAnotherPopup(this);"> ' % \
-            (self.related_url, name))
-        output.append('<img src="%sadmin/img/icon-addlink.svg" width="20" height="20" alt="%s"/></a>' % (settings.STATIC_URL, 'Add Another'))
-        return mark_safe(''.join(output))
-
-
 class SplitDurationWidget(forms.MultiWidget):
     """
     A Widget that splits duration input into two number input boxes.
